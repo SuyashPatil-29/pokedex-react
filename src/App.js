991cc5a9d98c4b5c1c1a14407793d645ed47074f
SuyashPatil-29 <112413583+SuyashPatil-29@users.noreply.github.com>
2023-03-04 11:57:41 +0530

    fetched pokemon api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  
+  const [pokemons, setPokemons] = useState([])
+  
+  useEffect(()=>{
+    fetch("https://pokeapi.co/api/v2/pokemon/")
+    .then(response=>response.json())
+    .then(json=>setPokemons(json.results))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {pokemons.map(pokemon=><h1>{pokemon.name}</h1>)}
     </div>
   );
 }
+
+
+
+
+
 
 export default App;
